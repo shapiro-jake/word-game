7bc2be9b6234b9eef80528a59f9a9263936bf041
Jacob Shapiro <jacobshp@mit.edu>
2022-06-22 20:03:01 -0700

    Wrote initial tests for WebServer

--- test/serverTest.ts
+++ test/serverTest.ts
@@ -27,9 +27,8 @@ describe('WebServer', () => {
      *   /submit/PlayerID/word
      *     - word: is valid, invalid
      *     - word: does not match, matches
-     *     - Number of players: PlayerID is only player playing, 
-     *                          second player is registered but has not submitted,
-     *                          second player has already submitted
+     *     - Number of players registered: 0, 1, 2
+     * 
      *   /playAgain/playerID
      *     - PlayerID: has just won, has not just won
      */
@@ -99,5 +98,90 @@ describe('WebServer', () => {
         assert.strictEqual(playAgainResponse.status, HttpStatus.NOT_ACCEPTABLE);
 
         server.stop();
+    });
+
+    it('covers a player submitting a word with only themselves playing', async function () {
+        const playerID = 'BobSmith'
+        const word = 'cat'
+        
+        const server = await startServer(0);
+        const registerURL = `http://localhost:${server.port}/register/${playerID}`
+        const registerResponse = await fetch(registerURL);
+        assert.strictEqual(await registerResponse.text(), `Registered ${playerID}`);
+
+        const submitURL = `http://localhost:${server.port}/submit/${playerID}/${word}`
+        const submitResponse = await fetch(submitURL);
+        assert.strictEqual(await submitResponse.text(), `${playerID} is the only player playing!`);
+        assert.strictEqual(submitResponse.status, HttpStatus.NOT_ACCEPTABLE);
+
+        server.stop(); 
+    });
+
+    it('covers two players submitting non-matching words', async function () {
+        const player1ID = 'BobSmith'
+        const player2ID = 'JackJohn'
+
+        const word1 = 'cat'
+        const word2 = 'dog'
+        
+        const server = await startServer(0);
+
+        const registerURL1 = `http://localhost:${server.port}/register/${player1ID}`
+        const registerResponse1 = await fetch(registerURL1);
+        assert.strictEqual(await registerResponse1.text(), `Registered ${player1ID}`);
+
+        const registerURL2 = `http://localhost:${server.port}/register/${player2ID}`
+        const registerResponse2 = await fetch(registerURL2);
+        assert.strictEqual(await registerResponse2.text(), `Registered ${player2ID}`);
+
+        const submitURL1 = `http://localhost:${server.port}/submit/${player1ID}/${word1}`
+        const submitResponse1 = await fetch(submitURL1);
+        assert.strictEqual(await submitResponse1.text(), `${player1ID} submitted: ${word1}`);
+
+
+        const submitURL2 = `http://localhost:${server.port}/submit/${player2ID}/${word2}`
+        const submitResponse2 = await fetch(submitURL2);
+        assert.strictEqual(await submitResponse2.text(), `${player2ID} submitted \'${word2}\' \
+            while ${player1ID} submitted \'${word1}\', which do not match. Try again!`);
+
+        const playAgainURL = `http://localhost:${server.port}/playAgain/${player1ID}/`;
+        const playAgainResponse = await fetch(playAgainURL);
+        assert.strictEqual(await playAgainResponse.text(), `${player1ID} cannot play again, they have not won!`);
+        assert.strictEqual(playAgainResponse.status, HttpStatus.NOT_ACCEPTABLE);
+
+        server.stop(); 
+    });
+
+    it('covers two players submitting matching words', async function () {
+        const player1ID = 'BobSmith'
+        const player2ID = 'JackJohn'
+
+        const word = 'cat'
+        
+        const server = await startServer(0);
+
+        const registerURL1 = `http://localhost:${server.port}/register/${player1ID}`
+        const registerResponse1 = await fetch(registerURL1);
+        assert.strictEqual(await registerResponse1.text(), `Registered ${player1ID}`);
+
+        const registerURL2 = `http://localhost:${server.port}/register/${player2ID}`
+        const registerResponse2 = await fetch(registerURL2);
+        assert.strictEqual(await registerResponse2.text(), `Registered ${player2ID}`);
+
+        const submitURL1 = `http://localhost:${server.port}/submit/${player1ID}/${word}`
+        const submitResponse1 = await fetch(submitURL1);
+        assert.strictEqual(await submitResponse1.text(), `Both players submitted \'${word}\'! Congratulations!`);
+
+
+        const submitURL2 = `http://localhost:${server.port}/submit/${player2ID}/${word}`
+        const submitResponse2 = await fetch(submitURL2);
+        assert.strictEqual(await submitResponse2.text(), `${player2ID} submitted: ${word}`);
+
+        const playAgainURL = `http://localhost:${server.port}/playAgain/${player1ID}/`;
+        const playAgainResponse = await fetch(playAgainURL);
+        assert.strictEqual(await playAgainResponse.text(), `${player1ID} has decided to play again!`);
+        assert.strictEqual(playAgainResponse.status, HttpStatus.NOT_ACCEPTABLE);
+
+        server.stop(); 
     });
 });
